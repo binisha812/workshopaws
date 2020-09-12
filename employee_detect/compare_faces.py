b59669b8b658e54f7ca32ed283af49318c60d892
binisha812 <71159726+binisha812@users.noreply.github.com>
2020-09-12 15:41:43 +0545

    Update compare_faces.py

--- employee_detect/compare_faces.py
+++ employee_detect/compare_faces.py
@@ -11,3 +11,21 @@ response = rekognition_client.detect_faces(
 
 )
 print(json.dumps(response, indent=4, sort_keys=True))
+
+if len(response["FaceDetails"]) > 0:
+    res = rekognition_client.search_faces_by_image(
+        CollectionId=CollectionId,
+        Image={
+            'Bytes': file
+        },
+
+
+    )
+    if len(res["FaceMatches"]) > 0:
+        print(res["FaceMatches"][0]["Face"]["ExternalImageId"])
+    else:
+        print("Doesnt exist")
+
+
+else:
+    print("No face detected")
