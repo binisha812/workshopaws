5031bb5ba6d22df125e6fed35993d692c90433cc
binisha812 <71159726+binisha812@users.noreply.github.com>
2020-09-12 12:53:41 +0545

    Add files via upload

---
+++ find_txt_image/text_image.py
@@ -0,0 +1,17 @@
+import boto3
+# import base64
+import json
+
+file = open('', 'rb').read()
+
+rekognition_client = boto3.client('rekognition')
+# rekognition_client = boto3.client('rekognition', aws_session_token="",
+#                                aws_access_key_id = "", aws_secret_access_key = "", region_name = 'us-east-1')
+
+
+response = rekognition_client.detect_text(
+    Image={
+        'Bytes': file
+    }
+)
+print(json.dumps(response, indent=4, sort_keys=True))
